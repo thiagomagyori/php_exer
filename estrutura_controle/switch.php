0bde5a57a41e2a188c8b72c487a8e48d929a112c
Thiago Magyori <thiagosilvamagyori@mail.com>
2023-02-25 23:47:47 -0300

    estrutura repetição

--- estrutura_controle/switch.php
+++ estrutura_controle/switch.php
@@ -1,35 +1,111 @@
 
 <?php
+
+// estrutura de condição
+
 /*ele sempre vai checar uma variavel*/
 
 
+// o break serve para quando um case for executado e para a execução e não executa o restante
 
-
-$tabuada = 2;
+$tabuada = 9;
 
 switch ($tabuada) {
-    case 0;
+    case 1;
         echo "Tabuada do 1";
         break;
 
-        case 1;
+        case 2;
         echo "Tabuada do 2";
         break;
 
-        case 2;
+        case 3;
         echo "Tabuada do 3";
         break;
+
+        case 4;
+        echo "Tabuada do 4";
+        break;
+
+        case 5;
+        echo "Tabuada do 5";
+        break;
+
+        case 6;
+        echo "Tabuada do 6";
+        break;
     
+    
+    // default é usado quando nenhuma condição for satisfeita ai entra no default
     default:
     echo "Tabuada não encontrada!";
 }
 
 
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
 
 
 
+echo "JOGO USANDO SWITCH";
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$sorteio = rand (1,10); // aqui rand vai sortear um numero aleatorio e jogar na variavel sorteio
 
 
+switch ($sorteio) {
+
+case 1;
+echo"Não foi dessa vez tente novamente"; // no case pode colocar um bloco de codigo para ser executado
+break;
+
+case 2;
+echo"Passou perto tente novamente";
+break;
+
+case 3;
+echo"Da proxima vez você vai ganhar";
+break;
+
+case 4;
+echo"Não desista você é capaz de vencer esse jogo";
+break;
+
+case 5;
+echo"Parabens você ganhou 1 milhão";
+break;
+
+case 6;
+echo"Se é pé frio tenta novamente";
+break;
+
+case 7;
+echo"Não foi dessa vez tente novamente";
+break;
+
+case 8;
+echo"Você ganhou 30 reais";
+break;
+
+case 9;
+echo"Você ganhou um misto quente";
+break;
+
+
+case 10;
+echo"Não foi dessa vez tente novamente";
+break;
+
+
+}
 
 
 
---
+++ estrutura_repetição/while.php
@@ -0,0 +1,63 @@
+<?php
+
+// while (enquanto) condição for verdadeira ele vai executando o loop
+
+
+
+
+
+//contador inicia valendo 0
+$contador = 0;
+
+
+
+//enquanto condição
+while ($contador < 10) {
+
+    echo $contador . "<br>";
+
+    //acrescenta mais 1
+    $contador ++;           //incremento
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+//inicia com 10
+$contador2 = 10;
+//enquanto   condição
+while ($contador2 > 0) {
+
+    echo $contador2 . "<br>";
+
+    //acrescenta mais 1
+    $contador2 --;          //decremento
+}
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+//contador inicia valendo 1
+$contador3 = 1;
+$tabuada = 4;
+
+
+//enquanto condição
+while ($contador3 <= 10) {
+   
+    echo $tabuada * $contador3 . "<br>";
+
+    //acrescenta mais 1
+    $contador3 ++;           //incremento
+}
+
+
+
+
+
+?>
